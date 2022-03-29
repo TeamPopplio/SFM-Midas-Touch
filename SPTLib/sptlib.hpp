43d7412a18c6ed107b9f2761c730e95ea2af1980
TeamPopplio <teampopplio@protonmail.com>
2022-03-29 16:44:49 -0700

    Add modified SPTLib.

---
+++ SPTLib/sptlib.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+#define SPTLIB_VERSION "1.2"
+
+// KiwifruitDev: I've replaced the engine messages with nothing.
+// This is because these messages will cause a crash.
+
+#define _EngineMsg(...)		   {};
+#define _EngineDevMsg(...)     {};
+#define _EngineWarning(...)    {};
+#define _EngineDevWarning(...) {};
+
+#ifndef SPT_MESSAGE_PREFIX
+#define SPT_MESSAGE_PREFIX "SPTLib: "
+#endif
+
+#define EngineMsg(...)         {};
+#define EngineDevMsg(...)      {};
+#define EngineWarning(...)     {};
+#define EngineDevWarning(...)  {};
+
+std::wstring::size_type GetRightmostSlash( const std::wstring& str, std::wstring::size_type pos = std::wstring::npos );
+std::wstring GetFileName( const std::wstring& fileNameWithPath );
+std::wstring GetFolderName( const std::wstring& fileNameWithPath );
+std::wstring NormalizePath(const std::wstring& path);
+std::wstring Convert(const std::string& from);
+std::string Convert(const std::wstring& from);
